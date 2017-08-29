0e4acc282fb818e6573589f6226fc5e7b781ae77
donkey_hotei <donkey-hotei@cryptolab.net>
2017-08-28 23:30:54 -0700

    3.58

--- homework/chapter_three/3_58.c
+++ homework/chapter_three/3_58.c
@@ -14,23 +14,40 @@ typedef enum { MODE_A, // 0
 } mode_t;
 
 // p1 @ ebp + 8, p2 @ ebp + 12, action @ ebp + 16
+// result in edx (intialized to -1)
 int switch3(int * p1, int * p2, mode_t action) {
-    int result = 0;
+    int result = -1;
 
     switch ( action ) {
         case MODE_A:
+            result = *p1;
+            *p1 = *p2;
             break;
         case MODE_B:
+            result = *p2;
+            *p2 = *p1;
             break;
         case MODE_C:
+            *p2 = 15;
+            result = *p1;
             break;
         case MODE_D:
+            *p2 = *p1;
+            result = 17;
             break;
         case MODE_E:
+            result = 17;
             break;
         default:
             break;
     }
 
     return result;
+}
+
+int main() {
+    int * p1;
+    int * p2;
+    mode_t action = MODE_A;
+    switch3(p1, p2, action);
 }
